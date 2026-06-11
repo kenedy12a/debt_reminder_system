98ae0a493eefc6413998593ca522f5188c06c904
kenedy12a <cyizakenedy2024@gmail.com>
2026-06-11 23:54:17 +1000

    fronted

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './index.css'
+import Register from "./components/register";
+
+function App(){
+  return(
+    <div>
+      <Register />
+    </div>
+  );
+}
+export default App;
